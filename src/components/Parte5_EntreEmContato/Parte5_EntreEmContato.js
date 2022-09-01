e37a56c6a575451576386d1cb6858cf76c598879
Leandro Patrício <leandro27patricio@gmail.com>
2022-08-31 21:12:32 -0300

    dia 17

--- src/components/Parte5_EntreEmContato/Parte5_EntreEmContato.js
+++ src/components/Parte5_EntreEmContato/Parte5_EntreEmContato.js
@@ -1,42 +1,35 @@
 import "./Parte5_EntreEmContato.css";
 
+import yahoo from "../../fig/yahoo.svg";
+import gmail from "../../fig/gmail.svg";
+import outlook from "../../fig/outlook.svg";
+
 const Parte5_EntreEmContato = () => {
-  const enviarEmailGmail = () => {
-    const assunto = document.getElementById("assunto").value;
-    const corpoDoTexto = document.getElementById("corpoDoTexto").value;
-    const link =
-      "https://mail.google.com/mail/u/0/?fs=1&tf=cm" +
-      "&to=leandro27patricio@gmail.com" +
-      "&su=" +
-      encodeURIComponent(assunto) +
-      "&body=" +
-      encodeURIComponent(corpoDoTexto);
+  const modal = document.getElementsByClassName("containerModal")[0];
 
-    window.open(link);
+  const abrirModal = () => {
+    modal.style.display = "block";
   };
-  const enviarEmailOutlook = () => {
-    const assunto = document.getElementById("assunto").value;
-    const corpoDoTexto = document.getElementById("corpoDoTexto").value;
-    const link =
-      "https://outlook.live.com/mail/0/deeplink/compose?popoutv2=1" +
-      "&to=leandro27patricio@gmail.com" +
-      "&subject=" +
-      encodeURIComponent(assunto) +
-      "&body=" +
-      encodeURIComponent(corpoDoTexto);
+  const fecharModal = () => {
+    modal.style.display = "none";
+  };
+  window.onclick = function (e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 
-    window.open(link);
-  };
-  const enviarEmailYahoo = () => {
-    const assunto = document.getElementById("assunto").value;
-    const corpoDoTexto = document.getElementById("corpoDoTexto").value;
-    const link =
-      "http://compose.mail.yahoo.com/" +
-      "&to=leandro27patricio@gmail.com" +
-      "&subject=" +
-      encodeURIComponent(assunto) +
-      "&body=" +
-      encodeURIComponent(corpoDoTexto);
+  const enviarEmail = (email) => {
+    let link = "";
+
+    if (email === "gmail") {
+      link = "https://mail.google.com/mail/u/0/?fs=1&tf=cm";
+    } else if (email === "outlook") {
+      link = "https://outlook.live.com/mail/0/deeplink/compose?popoutv2=1";
+    } else if (email === "yahoo") {
+      link = "http://compose.mail.yahoo.com/";
+    }
+    link += "&to=leandro27patricio@gmail.com";
 
     window.open(link);
   };
@@ -44,8 +37,8 @@ const Parte5_EntreEmContato = () => {
   return (
     <section className="secProjetos">
       <h1
-        name="projetos"
-        id="projetos"
+        name="entreEmContato"
+        id="entreEmContato"
         className="subTitulo"
         data-aos="fade-down"
         data-aos-duration="3000"
@@ -55,11 +48,29 @@ const Parte5_EntreEmContato = () => {
 
       <div>
         <div>Entre em contato comigo!</div>
-        <textarea id="assunto">Assunto do email</textarea>
-        <textarea id="corpoDoTexto">Corpo do texto</textarea>
-        <button onClick={() => enviarEmailGmail()}>Enviar gmail</button>
-        <button onClick={() => enviarEmailOutlook()}>Enviar outlook</button>
-        <button onClick={() => enviarEmailYahoo()}>Enviar yahoo</button>
+        <div className="abreModal" onClick={() => abrirModal()}>
+          Clique aqui para abrir o modal
+        </div>
+        <div className="containerModal">
+          <div className="conteudoModal">
+            Escolha um email de preferência{" "}
+            <span className="fechaModal" onClick={() => fecharModal()}>
+              &times;
+            </span>
+            <button onClick={() => enviarEmail("gmail")} className="btnEmail">
+              <img src={gmail} alt="gmail" className="imgEmail" />
+              Enviar pelo gmail
+            </button>
+            <button onClick={() => enviarEmail("outlook")} className="btnEmail">
+              <img src={outlook} alt="outlook" className="imgEmail" />
+              Enviar pelo outlook
+            </button>
+            <button onClick={() => enviarEmail("yahoo")} className="btnEmail">
+              <img src={yahoo} alt="yahoo" className="imgEmail" />
+              Enviar pelo yahoo
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
